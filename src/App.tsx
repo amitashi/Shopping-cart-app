3917c689ee8d5edfa7f77098d75ab45fa0af81e9
Amit Kumar Amit Kumar (Amit) <AmitKumar.AmitKumar@rockwellautomation.com>
2024-08-01 19:00:37 +0530

    started home page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Page/Home'
 
 const App = () => {
   return (
-    <h1>
+    <BrowserRouter>
       Shopping Cart
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
---
+++ src/Page/Home.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <Box>
+      Home Page
+    </Box>
+  )
+}
+
+export default Home
